3838b497f1628825cab5f80a452dead32ca9fce1
AshishBharodiya <ashishpatel3025@gmail.com>
2024-12-18 19:57:41 +0530

    first commit

--- Backends/server.js
+++ Backends/server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(cors())
 //  app.use(express.urlencoded({ extended: true }));
 
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: true }));
+//  app.use(bodyParser.json());
+//  app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/uploads", express.static("uploads"));
 
 app.use('/api',router);
