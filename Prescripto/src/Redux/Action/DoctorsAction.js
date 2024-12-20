a0240fe9a605f09cfbddb62083b37a1628e6f9e3
Ashishbharodiya <ashishpatel3025@gmail.com>
2024-12-20 12:37:38 +0530

    Update DoctorsAction.js
    First commit

--- Prescripto/src/Redux/Action/DoctorsAction.js
+++ Prescripto/src/Redux/Action/DoctorsAction.js
@@ -3,7 +3,7 @@ import { GETDOCTORSDETAL } from "../Type/type";
 
 export const DoctorDetailAction = (Atoken) => {
     return (dispatch) => {
-        axios.get("http://localhost:5000/api/admin/all-doctors", {
+        axios.get("https://prescripto-62tm.onrender.com/api/admin/all-doctors", {
             headers: { Authorization: `Bearer ${Atoken}` }
         }).then((res) => {
             console.log(res.data);
