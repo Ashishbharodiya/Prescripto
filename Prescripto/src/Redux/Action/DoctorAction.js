d298a79c1a0d5dcbe5405378d9c3f30e8eef9936
Ashishbharodiya <ashishpatel3025@gmail.com>
2024-12-23 17:55:02 +0530

    Update DoctorAction.js

--- Prescripto/src/Redux/Action/DoctorAction.js
+++ Prescripto/src/Redux/Action/DoctorAction.js
@@ -4,7 +4,7 @@ import { GETDOCTORDETAIL, GETDOCTORSDETAL, GETUSERDETAIL } from "../Type/type";
 
 export const DoctorDetailAction = (Dtoken)=> {
     return  (dispatch )=> {
-        axios.get("https://prescripto-62tm.onrender.com/api/doctor/profile",Dtoken).then((res)=> {
+        axios.get("https://prescripto-7.onrender.com/api/doctor/profile",Dtoken).then((res)=> {
             dispatch({type:GETDOCTORDETAIL,data1:res.data.data});
         })
     }
