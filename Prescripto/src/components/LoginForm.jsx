d553a0b1df1d41a7c0d04d1016344cf98869c83c
Ashishbharodiya <ashishpatel3025@gmail.com>
2024-12-23 19:09:52 +0530

    Update LoginForm.jsx

--- Prescripto/src/components/LoginForm.jsx
+++ Prescripto/src/components/LoginForm.jsx
@@ -18,7 +18,7 @@ function LoginForm() {
     e.preventDefault();  
 
     try {
-      const response = await axios.post('https://prescripto-1-qo8n.onrender.com/api/user/login', Loginobj, {
+      const response = await axios.post('https://prescripto-3-ry9r.onrender.com/api/user/login', Loginobj, {
         headers: {
           'Content-Type': 'application/json', 
         },
