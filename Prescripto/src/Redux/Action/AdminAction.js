11bbfe55a5f88ecc268a3719a3b3793684a5ef53
Ashishbharodiya <ashishpatel3025@gmail.com>
2024-12-27 19:46:17 +0530

    Update AdminAction.js

--- Prescripto/src/Redux/Action/AdminAction.js
+++ Prescripto/src/Redux/Action/AdminAction.js
@@ -4,7 +4,7 @@ export const GetloginAction = (formData, Atoken) => {
   return async (dispatch) => {
     try {
       const response = await axios.post(
-        "https://prescripto-3-ry9r.onrender.com/api/admin/registration", 
+        "https://prescripto-66h4.onrender.com/api/admin/registration", 
         formData, 
         Atoken 
       );
